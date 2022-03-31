1c8c85ec97c824951258579fff1970d92fddfc41
Patrick Noda <patricknoda11@gmail.com>
2022-03-31 00:15:02 -0700

    implemented create listing functionality on webpage

--- client/src/components/CreateListingTab.js
+++ client/src/components/CreateListingTab.js
@@ -1,104 +1,182 @@
 import { useState } from "react";
 
 const CreateListingTab = () => {
-    let [numBathrooms, setNumBathrooms] = useState("");
-    let [interiorSize, setInteriorSize] = useState("");
-    let [landSize, setLandSize] = useState("");
-    let [startingPrice, setStartingPrice] = useState("");
-    let [maximumPrice, setMaximumPrice] = useState("");
+  let [ownerPhoneNumber, setOwnerPhoneNumber] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [listingAddress, setListingAddress] = useState("");
+  const [agentEmail, setAgentEmail] = useState("");
+  let [price, setPrice] = useState("");
+  const [location, setLocation] = useState("");
+  const [type, setType] = useState("");
+  let [numberOfRooms, setNumberOfRooms] = useState("");
+  let [numberOfBathrooms, setNumberOfBathrooms] = useState("");
+  let [interiorSize, setInteriorSize] = useState("");
+  let [landSize, setLandSize] = useState("");
 
-    const clearEntries = () => {
-        setNumBathrooms("");
-        setInteriorSize("");
-        setLandSize("");
-        setStartingPrice("");
-        setMaximumPrice("");
-    };
+  const clearEntries = () => {
+    setOwnerPhoneNumber("");
+    setOwnerName("");
+    setListingAddress("");
+    setAgentEmail("");
+    setPrice("");
+    setLocation("");
+    setType("");
+    setNumberOfRooms("");
+    setNumberOfBathrooms("");
+    setInteriorSize("");
+    setLandSize("");
+  };
 
-    const onSubmitForm = async (e) => {
-        e.preventDefault();
-        try {
-            const body = {
-                numBathrooms,
-                interiorSize,
-                landSize,
-                startingPrice,
-                maximumPrice,
-            };
-            await fetch("http://localhost:5013/user/listing/", {
-                method: "POST",
-                mode: "cors",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(body),
-            });
-            alert("listing was successfully searched");
-            clearEntries();
-        } catch (error) {
-            alert(error.message);
-            clearEntries();
-        }
-    };
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = {
+        ownerPhoneNumber,
+        ownerName,
+        listingAddress,
+        agentEmail,
+        price,
+        location,
+        type,
+        numberOfRooms,
+        numberOfBathrooms,
+        interiorSize,
+        landSize,
+      };
+      await fetch("http://localhost:5013/listings/", {
+        method: "POST",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      alert("The listing specified has been created");
+      clearEntries();
+    } catch (error) {
+      alert(error.message);
+      clearEntries();
+    }
+  };
 
-    return (
-        <div className="flex-container-agent">
-            <h1>Search Listings</h1>
-            <div className="content">
-                <form onSubmit={onSubmitForm}>
-                    <div className="form-group">
-                        <label>Number of Bathrooms</label>
-                        <input
-                            type="number"
-                            placeholder="e.g. 3"
-                            className="form-control"
-                            value={numBathrooms}
-                            onChange={(e) => setNumBathrooms(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Interior Size</label>
-                        <input
-                            type="email"
-                            placeholder="e.g. 115"
-                            className="form-control"
-                            value={interiorSize}
-                            onChange={(e) => setInteriorSize(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Land Size </label>
-                        <input
-                            type="number"
-                            placeholder="e.g. 125"
-                            className="form-control"
-                            value={landSize}
-                            onChange={(e) => setLandSize(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Starting Price</label>
-                        <input
-                            type="number"
-                            placeholder="e.g. 1000000"
-                            className="form-control"
-                            value={startingPrice}
-                            onChange={(e) => setStartingPrice(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Maxim Price</label>
-                        <input
-                            type="number"
-                            placeholder="e.g. 10000000"
-                            className="form-control"
-                            value={maximumPrice}
-                            onChange={(e) => setMaximumPrice(e.target.value)}
-                        />
-                    </div>
-                    <button className="btn btn-primary">search</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex-container-agent">
+      <h1>Create Listing</h1>
+      <div className="content">
+        <form onSubmit={onSubmitForm}>
+          <div className="form-group">
+            <label>Owner Phone Number</label>
+            <input
+              type="number"
+              placeholder="e.g. 3"
+              className="form-control"
+              value={ownerPhoneNumber}
+              onChange={(e) => setOwnerPhoneNumber(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Owner Name</label>
+            <input
+              type="text"
+              placeholder="e.g. Dakota Johnson"
+              className="form-control"
+              value={ownerName}
+              onChange={(e) => setOwnerName(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Listing Address</label>
+            <input
+              type="text"
+              placeholder="e.g. 12345 Alma St"
+              className="form-control"
+              value={listingAddress}
+              onChange={(e) => setListingAddress(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Agent Email</label>
+            <input
+              type="email"
+              placeholder="e.g. 1000000"
+              className="form-control"
+              value={agentEmail}
+              onChange={(e) => setAgentEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Price</label>
+            <input
+              type="number"
+              placeholder="e.g. 1000000"
+              className="form-control"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Location</label>
+            <input
+              type="text"
+              placeholder="e.g. Vancouver, BC"
+              className="form-control"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Type</label>
+            <input
+              type="text"
+              placeholder="e.g. Duplex"
+              className="form-control"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Number Of Bedrooms</label>
+            <input
+              type="number"
+              placeholder="e.g. 5"
+              className="form-control"
+              value={numberOfRooms}
+              onChange={(e) => setNumberOfRooms(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Number of Bathrooms</label>
+            <input
+              type="number"
+              placeholder="e.g. 4"
+              className="form-control"
+              value={numberOfBathrooms}
+              onChange={(e) => setNumberOfBathrooms(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Interior Size (sq-ft)</label>
+            <input
+              type="number"
+              placeholder="e.g. 5000"
+              className="form-control"
+              value={interiorSize}
+              onChange={(e) => setInteriorSize(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Land Size (sq-ft)</label>
+            <input
+              type="number"
+              placeholder="e.g. 10000"
+              className="form-control"
+              value={landSize}
+              onChange={(e) => setLandSize(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-primary">Create</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CreateListingTab;
--- client/src/components/Listings.js
+++ client/src/components/Listings.js
@@ -2,29 +2,27 @@ import "./styles/Listings.scss";
 import { useState } from "react";
 import { PropTypes } from "prop-types";
 import { Box, Tab, Tabs, Typography } from "@mui/material";
-import UpdateListingTab from "./UpdateListingTab"
-import GetListingTab from "./GetListingTab"
-import CreateListingTab from "./CreateListingTab"
-
-// TODO: Implement this
+import UpdateListingTab from "./UpdateListingTab";
+import GetListingTab from "./GetListingTab";
+import CreateListingTab from "./CreateListingTab";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
   return (
-      <div
-          role="tabpanel"
-          hidden={value !== index}
-          id={`simple-tabpanel-${index}`}
-          aria-labelledby={`simple-tab-${index}`}
-          {...other}
-      >
-        {value === index && (
-            <Box sx={{ p: 3 }}>
-              <Typography>{children}</Typography>
-            </Box>
-        )}
-      </div>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
   );
 }
 
@@ -47,28 +45,27 @@ const Agent = () => {
     setTab(newValue);
   };
   return (
-      <div className="flex-container-agent">
-        <Box sx={{ width: "100%" }}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <Tabs value={tab} onChange={handleChange}>
-              <Tab label="Create Listing" {...a11yProps(0)} />
-              <Tab label="Get Listing" {...a11yProps(1)} />
-              <Tab label="Update Listing" {...a11yProps(2)} />
-            </Tabs>
-          </Box>
-          <TabPanel value={tab} index={0}>
-            <CreateListingTab />
-          </TabPanel>
-          <TabPanel value={tab} index={1}>
-            <GetListingTab />
-          </TabPanel>
-          <TabPanel value={tab} index={2}>
-            <UpdateListingTab />
-          </TabPanel>
+    <div className="flex-container-agent">
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={tab} onChange={handleChange}>
+            <Tab label="Create Listing" {...a11yProps(0)} />
+            <Tab label="Get Listing" {...a11yProps(1)} />
+            <Tab label="Update Listing" {...a11yProps(2)} />
+          </Tabs>
         </Box>
-      </div>
+        <TabPanel value={tab} index={0}>
+          <CreateListingTab />
+        </TabPanel>
+        <TabPanel value={tab} index={1}>
+          <GetListingTab />
+        </TabPanel>
+        <TabPanel value={tab} index={2}>
+          <UpdateListingTab />
+        </TabPanel>
+      </Box>
+    </div>
   );
 };
 
 export default Agent;
-
