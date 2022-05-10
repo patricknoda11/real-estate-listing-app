c9d2b3a9d57269bdab5c9d7a98e1704d3ed12b49
Patrick Noda <patricknoda11@gmail.com>
2022-05-10 01:04:53 -0700

    adjusted scss for about view

--- server/database.sql
+++ server/database.sql
@@ -87,3 +87,86 @@ CREATE TABLE PropertyHas (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
+
+-- Test data Agent:
+--
+INSERT INTO Agent (phoneNumber, agentEmail, password, name, birthday, yearsExperience, preferredMeetingDuration, preferredInPersonMeetingLocation) VALUES (6046547895, 'romeolink@gmail.com', '!@#!$123', 'Romeo', '1997-05-28', 3, 30, 'Richmond, BC');
+
+INSERT INTO Agent (phoneNumber, agentEmail, password, name, birthday, yearsExperience, preferredMeetingDuration, preferredInPersonMeetingLocation) VALUES (7789897546, 'Samvolk@gmail.com', '!@#!$123', 'Sam Volk', '1996-09-01', 6, 30, 'Burnaby, BC');
+
+INSERT INTO Agent (phoneNumber, agentEmail, password, name, birthday, yearsExperience, preferredMeetingDuration, preferredInPersonMeetingLocation) VALUES (6045692134, 'williamvengence@gmail.com', '1234123', 'William Vengence', '1989-09-08', 8, 60, 'Vancouver, BC');
+
+INSERT INTO Agent (phoneNumber, agentEmail, password, name, birthday, yearsExperience, preferredMeetingDuration, preferredInPersonMeetingLocation) VALUES (7789453794, 'shanyasmith@gmail.com', 'shanyay901', 'Shanya Smith', '2001-03-22', 2, 45, 'Chilliwack, BC');
+
+INSERT INTO Agent (phoneNumber, agentEmail, password, name, birthday, yearsExperience, preferredMeetingDuration, preferredInPersonMeetingLocation) VALUES (2086457821, 'killianhayes@gmail.com', 'killian1901', 'Killian Hayes', '1995-01-02', 5, 60, 'Edmonton, AB');
+
+-- Test data ListingHas
+--
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('89762 89 st', 'patricknoda11@gmail.com', 200000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('01972 demko ave', 'patricknoda11@gmail.com', 800000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('12109 albert st', 'patricknoda11@gmail.com', 300000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('81921 broadway st', 'emmasmith@gmail.com', 700000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('29292 livingston st', 'emmasmith@gmail.com', 1100000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('13418 45 st', 'emmasmith@gmail.com', 890000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('59678 fairmont st', 'killianhayes@gmail.com', 150000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('90800 hive st', 'killianhayes@gmail.com', 2300000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('12345 smoothdrive st', 'kimberly@gmail.com', 780000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('56482 walnut ave', 'samturner@gmail.com', 200000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('16458 horshoe ave', 'samturner@gmail.com', 560000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('89754 snow ave', 'samturner@gmail.com', 888888);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('45687 lemon ave', 'samturner@gmail.com', 369000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('54672 cool st', 'taro@gmail.com', 965000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('15462 curry st', 'taro@gmail.com', 987200);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('12398 stone ave', 'williamvengence@gmail.com', 369000);
+
+INSERT INTO ListingHas (listingAddress, agentEmail, price) VALUES ('12457 hare ave', 'shanyasmith@gmail.com', 500000);
+
+-- Test data PropertyHas
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('89762 89 st', 'Squamish, BC', 'House', 4, 3, 7800, 9000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('01972 demko ave', 'North Vancouver, BC', 'Apartment', 2, 1, 2100, 2200);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('12109 albert st', 'North Vancouver, BC', 'Townhouse', 2, 3, 1800, 2500);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('81921 broadway st', 'North Vancouver, BC', 'House', 7, 3, 8500, 17000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('29292 livingston st', 'Vancouver, BC', 'House', 6, 3, 6000, 9060);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('13418 45 st', 'Chilliwack, BC', 'House', 4, 2, 3060, 5200);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('59678 fairmont st', 'Chilliwack, BC', 'House', 3, 2, 4600, 6000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('90800 hive st', 'Burnaby, BC', 'Condo', 3, 2, 2800, 4000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('12345 smoothdrive st', 'Vancouver, BC', 'House', 4, 3, 4800, 7200);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('56482 walnut ave', 'Vancouver, BC', 'House', 5, 3, 6000, 9000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('16458 horshoe ave', 'Richmond, BC', 'Townhouse', 3, 2, 3000, 4500);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('89754 snow ave', 'Richmond, BC', 'Apartment', 3, 1, 2000, 3000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('45687 lemon ave', 'Richmond, BC', 'House', 4, 3, 4900, 8000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('54672 cool st', 'Surrey, BC', 'House', 7, 5, 15000, 30000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('15462 curry st', 'Surrey, BC', 'House', 3, 2, 9000, 15000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('12398 stone ave', 'Richmond, BC', 'Townhouse', 3, 2, 6000, 7000);
+
+INSERT INTO PropertyHas (listingAddress, location, type, numberOfRooms, numberOfBathrooms, interiorSize, landSize) VALUES ('12457 hare ave', 'Vancouver, BC', 'Duplex', 8, 4, 12000, 22000);
