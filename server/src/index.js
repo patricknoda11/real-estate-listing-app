064e4276f97b5227953640779c0e18ba01f6e6e8
Patrick  Noda <patricknoda@Patricks-MacBook-Pro.local>
2023-11-23 01:35:54 -0800

    implemented controller/route to retrieve presigned url for file uploads to aws

--- server/src/index.js
+++ server/src/index.js
@@ -7,9 +7,10 @@ const listingsAnalyticsRoute = require('./routes/listings-analytics');
 const agentRoute = require('./routes/agent');
 const agentAnalyticsRoute = require('./routes/agent-analytics');
 const listingsRoute = require('./routes/listings');
+const s3Route = require('./routes/s3');
 
-const app = express();
 env.config();
+const app = express();
 
 const PORT = process.env.SERVER_PORT ?? 5012;
 
@@ -22,7 +23,8 @@ app.use('/listings/analytics', listingsAnalyticsRoute);
 app.use('/listings', listingsRoute);
 app.use('/user/agents/analytics', agentAnalyticsRoute);
 app.use('/user/agents', agentRoute);
+app.use('/s3/presigned-urls', s3Route);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.info(`Server running on port ${PORT}`);
 });
