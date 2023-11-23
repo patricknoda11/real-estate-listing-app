7b311273112bd71af909146abd09cd7d3580cc81
Patrick  Noda <patricknoda@Patricks-MacBook-Pro.local>
2023-11-23 03:02:08 -0800

    added uuid to dependency and implemented generate random id helper function

---
+++ server/src/utils/helpers.js
@@ -0,0 +1,7 @@
+const { v4: uuidv4 } = require('uuid');
+
+/**
+ * generates robust & universally unique identifiers
+ * @returns {string} - a unique id of 36 characters
+ */
+const generateUniqueId = () => uuidv4();
