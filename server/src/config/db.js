491ab994197860ffff113ff2bbbfdf28fa224a10
Patrick  Noda <patricknoda@Patricks-MacBook-Pro.local>
2023-11-23 02:29:16 -0800

    configure to utilize knex for database transactions

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,16 +1,23 @@
-const mysql = require('mysql2/promise');
+const knex = require('knex');
 const env = require('dotenv');
+
 env.config();
 
-const pool = mysql.createPool({
-	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
-	database: process.env.MYSQL_DATABASE,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PASSWORD,
-	connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
-	waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
-	queueLimit: process.env.MYSQL_QUEUE_LIMIT,
+const knexInstance = knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
+  },
+  pool: {
+    min: 0,
+    max: process.env.MYSQL_CONNECTION_LIMIT,
+    queueLimit: process.env.MYSQL_QUEUE_LIMIT
+  },
+  debug: false // set to true if you want to see SQL queries in console
 });
 
-module.exports = pool;
+module.exports = knexInstance;
